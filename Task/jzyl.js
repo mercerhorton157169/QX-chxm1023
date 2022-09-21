52af223fbb570e261af97f4987168f0b2b19186a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-22 00:15:45 +0800

    Update jzyl.js

--- Task/jzyl.js
+++ Task/jzyl.js
@@ -16,10 +16,10 @@
 
 [task_local]
 #健走有礼
-00 11,20 * * * https://raw.githubusercontent.com/chxm1023/QX/main/Task/jzyl.js, tag=健走有礼, enabled=true
+00 11,20 * * * https://raw.githubusercontents.com/chxm1023/QX/main/Task/jzyl.js, tag=健走有礼, enabled=true
 
 [rewrite_local]
-https://bwa.feierlaiedu.com/api/v1/bbg/taskRecord/queryUserInfo url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Task/jzyl.js
+https://bwa.feierlaiedu.com/api/v1/bbg/taskRecord/queryUserInfo url script-request-header https://raw.githubusercontents.com/chxm1023/QX/main/Task/jzyl.js
 
 [MITM]
 hostname = bwa.feierlaiedu.com
