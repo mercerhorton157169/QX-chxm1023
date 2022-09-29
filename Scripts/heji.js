91e3cb5b388abf161ff51b85e0ce34ed76658201
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-09-29 20:00:26 +0800

    Update heji.js

--- Scripts/heji.js
+++ Scripts/heji.js
@@ -1,7 +1,9 @@
+
+
 [MITM]
 hostname = %APPEND% buy.itunes.apple.com
 
 
 [Script]
-pandavip_Picsew_Pro = type=http-response,pattern= ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontents.com/chxm1023/script/main/JB/pandavip.js
+Tiao_fileball_Pro = type=http-response,pattern= ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontents.com/chxm1023/script/main/JB/pandavip.js
 RC = type=http-request,pattern= ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontents.com/chxm1023/script/main/JB/Picsew.js,script-update-interval=0
