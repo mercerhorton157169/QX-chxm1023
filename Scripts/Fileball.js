5439709c470fb653da12d0104f29c79966964e8e
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-04 10:17:33 +0800

    Update Fileball.js

--- Scripts/Fileball.js
+++ Scripts/Fileball.js
@@ -1,18 +1,33 @@
 {
+  "offerings" : [
+    {
+      "identifier" : "pro",
+      "packages" : [
+        {
+          "platform_product_identifier" : "filebox_pro",
+          "identifier" : "filebox_pro"
+        }
+      ],
+      "description" : "Pro to access all features"
+    },
+    {
+      "identifier" : "sale",
+      "packages" : [
+        {
+          "platform_product_identifier" : "filebox_pro",
+          "identifier" : "$rc_monthly"
+        }
+      ],
+      "description" : "just a test"
+    }
+  ],
+  "current_offering_id" : "pro"
+}
+{
   "request_date_ms" : 416505600000,
   "request_date" : "1983-03-15T00:00:00Z",
   "subscriber" : {
-    "entitlements" : {
-      "filebox_pro" : [
-        {
-          "id" : "yingzi",
-          "is_sandbox" : false,
-          "purchase_date" : "1983-03-15T00:00:00Z",
-          "original_purchase_date" : "1983-03-15T00:00:00Z",
-          "store" : "app_store"
-        }
-      ]
-    },
+    "last_seen" : "1983-03-15T00:00:00Z",
     "first_seen" : "1983-03-15T00:00:00Z",
     "original_application_version" : "170",
     "other_purchases" : {
@@ -26,14 +41,24 @@
     },
     "entitlements" : {
       "filebox_pro" : {
-        "grace_period_expires_date" : null,
+        "expires_date" : null,
         "purchase_date" : "1983-03-15T00:00:00Z",
         "product_identifier" : "filebox_pro",
-        "expires_date" : "2999-03-15T00:00:00Z"
+        "grace_period_expires_date" : null
       }
     },
     "original_purchase_date" : "1983-03-15T00:00:00Z",
-    "original_app_user_id" : "$RCAnonymousID:3A8e5a11e56b4246f2ab2b17058c01db1e",
-    "last_seen" : "1983-03-15T00:00:00Z"
+    "original_app_user_id" : "$RCAnonymousID%3A8e5a11e56b4246f2ab2b17058c01db1e",
+    "non_subscriptions" : {
+      "filebox_pro" : [
+        {
+          "id" : "chxm1023",
+          "is_sandbox" : false,
+          "purchase_date" : "1983-03-15T00:00:00Z",
+          "original_purchase_date" : "1983-03-15T00:00:00Z",
+          "store" : "app_store"
+        }
+      ]
+    }
   }
 }
