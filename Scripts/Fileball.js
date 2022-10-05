57bb13109ec0d34c34d139a3a512b27e398bb9d8
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-05 10:14:56 +0800

    Update Fileball.js

--- Scripts/Fileball.js
+++ Scripts/Fileball.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fileball.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontents.com/chxm1023/QX/main/Scripts/Fileball.js
 
 [mitm] 
 
