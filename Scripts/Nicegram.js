6ab1e0bd0277cf22a5112fe31e7db25e4632e378
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-08 21:39:54 +0800

    Update Nicegram.js

--- Scripts/Nicegram.js
+++ Scripts/Nicegram.js
@@ -1,6 +1,6 @@
 /*********************************************
 
-https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json script-echo-response https://raw.githubusercontent.com/chxm1023/QX/main/Scripts/Nicegram.js
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontents.com/chxm1023/QX/main/Scripts/Nicegram.js
 
 hostname= restore-access.indream.app
 
