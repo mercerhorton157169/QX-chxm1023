ab3c6160df050534e9bab65b8d58209647f21e1f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-08 22:51:18 +0800

    Update Nicegram.js

--- Scripts/Nicegram.js
+++ Scripts/Nicegram.js
@@ -5,9 +5,5 @@ https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-res
 hostname= restore-access.indream.app
 
 *********************************************/
-const obj = {
-  "data": {
-    "premiumAccess": true
-  }
-};
-$done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
+
+constobj={"data":{"premiumAccess":true}};$done({status:200,headers:{'Content-Type':'application/json'},body:JSON.stringify(obj)});
