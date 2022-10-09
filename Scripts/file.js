906db46bd78b1f90f84e795c046e198455d73356
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-09 19:53:35 +0800

    Update file.js

--- Scripts/file.js
+++ Scripts/file.js
@@ -6,7 +6,7 @@
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body fileball.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%3A\w{32})$ url script-response-body https://raw.githubusercontents.com/chxm1023/QX/main/Scripts/file.js
 
 [mitm] 
 
