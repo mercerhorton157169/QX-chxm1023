815b1ea36da438773a3253c18d49931305dc7305
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-18 14:20:40 +0800

    Update dns-clear-cache.js

--- Task/dns-clear-cache.js
+++ Task/dns-clear-cache.js
@@ -1,12 +1,3 @@
-/*********************************************
-
-项目名称：清理Dns缓存
-
-[task_local]
-0 */6 * * ? https://raw.githubusercontents.com/chxm1023/QX/main/Task/dns-clear-cache.js, tag=清理Dns缓存, img-url=https://raw.githubusercontents.com/chxm1023/script/main/icon/clean.png, enabled=true
-
-*********************************************/
-
 const message = {
     action: "dns_clear_cache"
 };
