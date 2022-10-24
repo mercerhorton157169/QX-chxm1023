c9c33840bd666a315cb60e01f35b9e5f84a3ba7c
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-24 10:44:19 +0800

    Create Reheji.js

---
+++ Scripts/Reheji.js
@@ -0,0 +1,38 @@
+/*****************************************
+
+使用方法：先开脚本再打开App，自动会解锁，如果没效果就关了重开或者按一下恢复购买，在还不行就卸载App重新安装！最后还不行的话就是脚本失效了！
+
+项目名称：Fileball 解锁Pro ✅
+下载地址：已下架，已购的还可下载
+
+项目名称：VSCO 解锁Pro功能 ✅
+下载地址：http://mtw.so/6kOidj
+
+项目名称：1Blocker 拦截工具✅
+下载地址：
+
+项目名称：图图记账 解锁Pro功能 ✅
+下载地址：http://mtw.so/5QIv3Y
+
+项目名称：手机硬件管家 解锁高级版 ✅
+下载地址：http://mtw.so/6cB8gn
+
+项目名称：Tiao 解锁高级版（1.5.4以下版本）✅
+下载地址：自己抓旧版吧，新版不行
+
+项目名称：Pillow 解锁Pro功能 ✅
+下载地址：http://mtw.so/6kOqx7
+
+项目名称：Scanner Pro 解锁高级Vip权限
+下载地址：http://mtw.so/6bSOyO
+
+
+*****************************************
+
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(\$RCAnonymousID%)?(\w)*$) url script-response-body ceshijs.js
+
+
+hostname = api.revenuecat.com
+
+
+*****************************************/
