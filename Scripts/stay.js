7df977bc9b46071fbbc18d01abebb8bec983b75c
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-31 02:12:13 +0800

    Update stay.js

--- Scripts/stay.js
+++ Scripts/stay.js
@@ -14,4 +14,9 @@ hostname = api.shenyin.name
 ****************************************/
 
 
+var chxm1023 = JSON.parse($response.body);
+
+    chxm1023 = 
 {"device_name":"iPhone","app_version":"2.3.4","country":"CN","uuid":"924B95ED-6E0E-4D0E-8C29-8D39295BB96F","os_version":"","device_type":"iphone","pro":"lifetime","is_extension":false}
+
+$done({body : JSON.stringify(chxm1023)});
