6a56c04ccbf8c5819f58a131db9fa342360612a6
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-31 02:09:40 +0800

    Create stay.js

---
+++ Scripts/stay.js
@@ -0,0 +1,17 @@
+
+/****************************************
+
+项目功能：stay 解锁Pro
+下载地址：
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
+
+^https:\/\/api\.shenyin\.name\/stay\/active url script-request-header https://raw.githubusercontent.com/chxm1023/QX/main/Scripts/stay.js
+
+hostname = api.shenyin.name
+
+****************************************/
+
+
+{"device_name":"iPhone","app_version":"2.3.4","country":"CN","uuid":"924B95ED-6E0E-4D0E-8C29-8D39295BB96F","os_version":"","device_type":"iphone","pro":"lifetime","is_extension":false}
