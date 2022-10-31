c39aff2c7dad8bee2a0cdb9cc488fe5659707952
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-10-31 14:10:40 +0800

    Create arajf.js

---
+++ Scripts/arajf.js
@@ -0,0 +1,28 @@
+/****************************************
+
+项目功能：傲软全家福 解锁VIP
+下载地址：http://mtw.so/5ukhax
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
+
+[rewrite_local]
+
+^https:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/chxm1023/QX/main/Scripts/arajf.js
+
+[mitm]
+
+hostname = gw.aoscdn.com
+
+****************************************/
+
+var body = $response.body; 
+var obj = JSON.parse(body); 
+
+obj.data.is_activated = 1;
+obj.data.expire_time = "9999-09-09 09:09:09";
+obj.data.durations = 100000;
+obj.data.expired_at = 253395492741;
+
+body = JSON.stringify(obj);
+$done({body});
