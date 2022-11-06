e54ea3ff7e4fd5615a2defc8ff5358f7bae6ce67
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-06 13:50:52 +0800

    Update Agenda.js

--- Scripts/Agenda.js
+++ Scripts/Agenda.js
@@ -24,11 +24,11 @@ var body = $response.body;
 var chxm1023 = JSON.parse(body); 
 
 chxm1023.licenseStatusMobile = 1;
-chxm1023.unlockExpiryMobile = 63114076800;
+chxm1023.unlockExpiryMobile = 4096408296;
 chxm1023.informUserOfPendingBonus = true;
 chxm1023.licenseStatus = 1;
 chxm1023. isSubscribed = true;
-chxm1023.unlockExpiry = 63114076800;
+chxm1023.unlockExpiry = 4096408296;
 chxm1023.isSubscribedMobile = true;
 chxm1023.universalPurchaseAvailable = true;
 
