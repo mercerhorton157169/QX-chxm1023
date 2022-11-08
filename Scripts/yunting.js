9458ed19d25d8ab4fb5d2cb136ee427afd02af93
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-11-08 12:47:03 +0800

    Update yunting.js

--- Scripts/yunting.js
+++ Scripts/yunting.js
@@ -26,7 +26,6 @@ const ytapi = "/ytapi.radio.cn";
 
 if ($request.url.indexOf(getway) != -1){body = $response.body.replace(/\"isVip":\d+/g, '\"isVip":0').replace(/\"needPay":\d+/g, '\"needPay":0');}
 
-if ($request.url.indexOf(ytapi) != -1){ body = $response.body.replace(/\"vipTime":"(.*?)"/g,'\"vipTime":"2099-10-23"');}
-
+if ($request.url.indexOf(ytapi) != -1){body = $response.body.replace(/\"vipTime":"(.*?)"/g,'\"vipTime":"2099-10-23"');}
 
 $done({body});
